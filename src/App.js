542117d99d332227b5ae09eae9379edfae459bff
Jonas <jonaso@web.de>
2017-03-25 12:24:17 +0100

    test morph picture into headline

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Overdrive from 'react-overdrive';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			loading: true
+		};
+	}
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({loading: false});
+		}, 2000);
+	}
+    render() {
+	    return (
+	      <div className="App">
+	        <div className="App-header">
+				{this.state.loading && <Overdrive id="content"><img src={logo} className="App-logo" alt="logo" /></Overdrive>}
+				{!this.state.loading && <Overdrive id="content">
+					<h2>React-Overdrive Test</h2>
+				</Overdrive>}
+	        </div>
+	      </div>
+	    );
+    }
 }
 
 export default App;
